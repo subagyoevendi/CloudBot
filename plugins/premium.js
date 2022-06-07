77d9d434b2f2195440fe659939a56d8eccfeeb2f
subagyoevendi <106004746+subagyoevendi@users.noreply.github.com>
2022-06-07 09:25:04 +0700

    Delete plugins directory

--- plugins/premium.js
+++
@@ -1,7 +0,0 @@
-let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `“Silahkan chat owner”`, author, ['ඩOwner', `${usedPrefix}owner`], m)
-
-handler.help = ['premium']
-handler.tags = ['quotes']
-handler.command = /^(premium)$/i
-
-export default handler
